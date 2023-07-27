6043c771dab8926354f1a656adf9b663a87a0480
zhangningle <1160030724@qq.com>
2023-07-27 11:41:26 +0800

    fix: prisma is not defind

--- app/api/todo/delete/route.ts
+++ app/api/todo/delete/route.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
